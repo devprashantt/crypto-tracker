7be3f466330cab1c62821777ff048ea3cf5a9179
Prashant <pk3076889@gmail.com>
2022-04-09 22:31:05 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/my_project13/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_project13
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/coin_data.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+const apiKey = '65B1A2FE-67AC-47B5-B24B-888C7DE879D4';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    //4: Use a for loop here to loop through the cryptoList and request the data for each of them in turn.
+    //5: Return a Map of the results instead of a single value.
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      //Update the URL to use the crypto symbol from the cryptoList
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency?apikey=$apiKey';
+      http.Response response = await http.get(
+        Uri.parse(requestURL)
+      );
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double lastPrice = decodedData['rate'];
+        //Create a new key value pair, with the key being the crypto symbol and the value being the lastPrice of that crypto currency.
+        cryptoPrices[crypto] = lastPrice.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
